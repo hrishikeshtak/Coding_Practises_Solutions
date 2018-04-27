613e69decb3254aa7b2704b122001519935da981
Chandramohan Jagtap <cmjagtap1@gmail.com>
2018-04-28 00:53:22 +0530

    Update Tuples.py

--- Python/Basic_Data_Types/Tuples.py
+++ Python/Basic_Data_Types/Tuples.py
@@ -1,13 +1,13 @@
 # Tuples
 # Task
-# Given an integer, , and space-separated integers as input, create a tuple, , of those integers. Then
+# Given an integer,n, and n space-separated integers as input, create a tuple,t , of those n integers. Then
 # compute and print the result of .
 # Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
 # Input Format
-# The first line contains an integer, , denoting the number of elements in the tuple.
-# The second line contains space-separated integers describing the elements in tuple .
+# The first line contains an integer,n , denoting the number of elements in the tuple.
+# The second line contains n space-separated integers describing the elements in tuple t.
 # Output Format
-# Print the result of .
+# Print the result of hash(t).
 # Sample Input 0
 # 2
 # 1 2
