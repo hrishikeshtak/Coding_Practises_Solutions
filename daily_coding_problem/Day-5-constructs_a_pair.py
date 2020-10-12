2e2433188c22fabe3f67141eaee01d1278ce77a9
hrishikesh <hrishikeshtak@gmail.com>
2020-10-12 16:52:02 +0530

    Renamed Dir Structure

--- daily_coding_problem/Day-5-constructs_a_pair.py
+++ daily_coding_problem/Day-5-constructs_a_pair.py
@@ -3,9 +3,16 @@
 """
 This problem was asked by Jane Street.
 
-cons(a, b) constructs a pair, and car(pair) and cdr(pair)
-returns the first and last element of that pair.
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair.
 For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
 """
 
 
@@ -15,27 +22,23 @@ def cons(a, b):
     return pair
 
 
-def func(a, b):
+def left(a, b):
     return a
 
 
-def func1(a, b):
+def right(a, b):
     return b
 
 
 def car(somefunction):
     # print(somefunction)
-    return somefunction(func)
+    return somefunction(left)
 
 
 def cdr(somefunction):
     # print(somefunction)
-    return somefunction(func1)
+    return somefunction(right)
 
-
-# a = cons(3, 4)
-# print(a(func))
-# print(a(func1))
 
 print(car(cons(3, 4)))
 print(cdr(cons(3, 4)))
